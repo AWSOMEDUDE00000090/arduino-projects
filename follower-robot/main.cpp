1c92b4d3c830f1a00d07263523c204d4b6fc2366
Eric <70622326+AWSOMEDUDE00000090@users.noreply.github.com>
2024-01-26 11:49:00 -0800

    Update main.cpp

--- follower-robot/main.cpp
+++ follower-robot/main.cpp
@@ -1,1 +1,12 @@
 #include <Arduino.h>
+
+// Function for 
+
+// Function for object not found
+
+// Function for finding object
+
+// Function for servo locking onto object
+
+// Function for turning robot straight keeping servo locked onto object
+
